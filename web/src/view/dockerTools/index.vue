0ecee09f03a5998d3e4883d68d4da3e37a8a05bf
bright.ma <bright.ma@magesfc.com>
2023-08-03 16:13:45 +0800

    web: dockerTools: add load getDockerVersion
    Change-Id: Iae4eb060d3593b8dfaaccbc2e5db7439571ddf47

--- web/src/view/dockerTools/index.vue
+++ web/src/view/dockerTools/index.vue
@@ -3,20 +3,36 @@
     <router-view v-if="$route.meta.keepAlive" v-slot="{ Component }">
       <transition mode="out-in" name="el-fade-in-linear">
         <keep-alive>
-          <component :is="Component" />
+          <component :is="Component"/>
         </keep-alive>
       </transition>
     </router-view>
     <router-view v-if="!$route.meta.keepAlive" v-slot="{ Component }">
       <transition mode="out-in" name="el-fade-in-linear">
-        <component :is="Component" />
+        <component :is="Component"/>
       </transition>
     </router-view>
   </div>
 </template>
 
 <script>
+import { getDockerVersion } from "@/api/docker";
+
 export default {
-  name: 'Docker'
+  name: 'Docker',
+  data() {
+    return {}
+  },
+  created() {
+    this.version()
+
+  },
+
+  methods: {
+    async version() {
+      const d = await getDockerVersion()
+      console.log(d)
+    },
+  }
 }
 </script>
