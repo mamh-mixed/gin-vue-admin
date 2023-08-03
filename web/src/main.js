7f5faf9a6b69068122c3982e3f20b3e3fc9f0945
bright.ma <bright.ma@magesfc.com>
2023-08-03 09:27:09 +0800

    web: main.js: add font-awesome
    Change-Id: I947616940df0587794c2c826f7c13ab0ef4a0a13

--- web/src/main.js
+++ web/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import 'element-plus/dist/index.css'
+import 'font-awesome/css/font-awesome.min.css'
 import ElementPlus from 'element-plus'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 // 引入gin-vue-admin前端初始化相关内容
