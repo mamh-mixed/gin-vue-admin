323c4bcb5b11c3d4f3aec6f0554b2c31656b762d
bright.ma <bright.ma@magesfc.com>
2023-08-03 16:16:02 +0800

    server: api/v1/system: add docker api
    Change-Id: I29897d1135fdd5b7dbba252555dd0654d4fb539c

---
+++ server/api/v1/system/sys_docker.go
@@ -0,0 +1,30 @@
+package system
+
+import (
+	"github.com/flipped-aurora/gin-vue-admin/server/global"
+	"github.com/flipped-aurora/gin-vue-admin/server/model/common/response"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+type DockerApi struct {
+}
+
+func (a *DockerApi) GetDockerVersion(c *gin.Context) {
+	if err, version := dockerService.GetDockerVersion(); err != nil {
+		global.GVA_LOG.Error("获取失败!", zap.Any("err", err))
+		response.FailWithMessage("获取失败", c)
+	} else {
+		response.OkWithDetailed(gin.H{"version": version}, "获取成功", c)
+	}
+
+}
+
+func (a *DockerApi) GetDockerInfo(c *gin.Context) {
+	if err, info := dockerService.GetDockerInfo(); err != nil {
+		global.GVA_LOG.Error("获取失败!", zap.Any("err", err))
+		response.FailWithMessage("获取失败", c)
+	} else {
+		response.OkWithDetailed(gin.H{"info": info}, "获取成功", c)
+	}
+}
