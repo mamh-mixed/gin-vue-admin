4fa5c69dbf76bead81d835ce4ec16eaa326f8785
bright.ma <bright.ma@magesfc.com>
2023-08-04 12:25:40 +0800

    web: api: add getImages
    Change-Id: I4b682156754125b55cd5f9b3452a91efcf93f920

--- web/src/api/docker.js
+++ web/src/api/docker.js
@@ -24,3 +24,10 @@ export const getUsages = () => {
     donNotShowLoading: true
   })
 }
+
+export const getImages = () => {
+  return service({
+    url: '/docker/getImages',
+    method: 'get',
+  })
+}
