c68cb9ffdef9db870f424ac7cf75b84926e6c5ac
bright.ma <bright.ma@magesfc.com>
2023-08-04 08:17:10 +0800

    web: dockerTools: add  ResourceUsage
    Change-Id: Ie0bd148b7e48205f6e5ba2a247cf81aa1581f64d

---
+++ web/src/view/dockerTools/portal/ResourceUsage/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <el-row :gutter="10">
+      <el-col :span="12">CPU使用率:</el-col>
+      <el-col :span="12">
+        <el-progress
+            type="line"
+            :percentage="+cpuUsages.toFixed(0)"
+            :color="colors"
+        />
+      </el-col>
+    </el-row>
+
+    <el-row :gutter="10">
+      <el-col :span="12">内存占用率:</el-col>
+      <el-col :span="12">
+        <el-progress
+            type="line"
+            :percentage="+memUsages.toFixed(0)"
+            :color="colors"
+        />
+      </el-col>
+    </el-row>
+
+    <el-row :gutter="10">
+      <el-col :span="12">磁盘占用率:</el-col>
+      <el-col :span="12">
+        <el-progress
+            type="line"
+            :percentage="+diskUsages.toFixed(0)"
+            :color="colors"
+        />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+
+import {mapGetters} from "vuex";
+
+export default {
+  name: 'ResourceUsage',
+  data() {
+    return {
+      diskUsages:23,
+      cpuUsages:45,
+      memUsages:67,
+      colors: [
+        {color: '#5cb87a', percentage: 20},
+        {color: '#e6a23c', percentage: 40},
+        {color: '#f56c6c', percentage: 80}
+      ]
+    }
+  },
+  computed: {
+    ...mapGetters('docker', ['info', 'version']),
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
