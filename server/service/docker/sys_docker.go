ef428f60f1f31bb2ab9e2d422458ca149a521b08
bright.ma <bright.ma@magesfc.com>
2023-08-04 08:33:05 +0800

    server: service/docker: add Docker GetDockerUsages
    Change-Id: I9e52734673eb155d13b4d922eb273d3a0fc8016a

--- server/service/docker/sys_docker.go
+++ server/service/docker/sys_docker.go
@@ -3,6 +3,10 @@ package docker
 import (
 	"context"
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
+	"github.com/shirou/gopsutil/mem"
+	"time"
 )
 
 type DockerService struct {
@@ -17,11 +21,28 @@ func (s *DockerService) GetDockerVersion() (error, interface{}) {
 	return err, version
 }
 
-func (s *DockerService) GetDockerInfo() (err error, info interface{}) {
-	version, err := global.GVA_DOCKER.Info(context.Background())
+func (s *DockerService) GetDockerInfo() (error, interface{}) {
+	info, err := global.GVA_DOCKER.Info(context.Background())
 	if err != nil {
 		return err, nil
 	}
 
-	return err, version
+	return err, info
+}
+
+func (s *DockerService) GetDockerUsages() (error, interface{}) {
+	info, err := global.GVA_DOCKER.Info(context.Background())
+	if err != nil {
+		return err, nil
+	}
+
+	vmem, _ := mem.VirtualMemory()
+	cpuPercent, _ := cpu.Percent(time.Second, false)
+	diskinfo, _ := disk.Usage(info.DockerRootDir) //指定某路径的硬盘使用情况
+
+	data := make(map[string]any)
+	data["cpu"] = cpuPercent
+	data["disk"] = diskinfo
+	data["memory"] = vmem
+	return nil, data
 }
