8b742d3d1e1dc396d4c5ffda4d0cc1004c59bc27
bright.ma <bright.ma@magesfc.com>
2023-08-04 09:05:28 +0800

    web: api: add donNotShowLoading
    Change-Id: Iba14b0fd6b0b6214b8611605bb1ff2109fd73298

--- web/src/api/docker.js
+++ web/src/api/docker.js
@@ -4,20 +4,23 @@ import service from '@/utils/request'
 export const getVersion = () => {
   return service({
     url: '/docker/getVersion',
-    method: 'get'
+    method: 'get',
+    donNotShowLoading: true
   })
 }
 
 export const getInfo = () => {
-    return service({
-        url: '/docker/getInfo',
-        method: 'get'
-    })
+  return service({
+    url: '/docker/getInfo',
+    method: 'get',
+    donNotShowLoading: true
+  })
 }
 
 export const getUsages = () => {
   return service({
     url: '/docker/getUsages',
-    method: 'get'
+    method: 'get',
+    donNotShowLoading: true
   })
 }
