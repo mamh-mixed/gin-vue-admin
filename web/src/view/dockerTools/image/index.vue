8f850ff372f8451cc8d2d8860db6b93b9b720c91
bright.ma <bright.ma@magesfc.com>
2023-08-05 08:45:56 +0800

    web: dockerTools: import ImageTable and ImageDetail
    Change-Id: Ic0b50b12001ac1512dc240a25c9f1c4930090d39

--- web/src/view/dockerTools/image/index.vue
+++ web/src/view/dockerTools/image/index.vue
@@ -1,99 +1,30 @@
 <template>
   <div>
 
-    <el-table :data="tableData" border row-key="ID" stripe style="width: 100%">
-      <el-table-column label="序号" width="width" type="index" />
-      <el-table-column label="IMAGE ID" width="200" prop="Id" />
-      <el-table-column label="REPOSITORY" width="width" prop="Repository" />
-      <el-table-column label="TAG" width="200" prop="Tag" />
-      <el-table-column label="CREATED" width="100" prop="Created" />
-      <el-table-column label="SIZE" width="100" prop="Size" />
-      <el-table-column prop="prop" label="操作" width="300">
-        <template #default="scope">
-          <el-button
-              size="small"
-              type="warning"
-              icon="el-icon-edit"
-              @click="handleEdit(scope.$index, scope.row)"
-          >查看
-          </el-button>
-          <el-button
-              size="small"
-              type="danger"
-              icon="el-icon-delete"
-              @click="handleDelete(scope.$index, scope.row)"
-          >运行
-          </el-button>
-          <el-button
-              size="small"
-              type="danger"
-              icon="el-icon-delete"
-              @click="handleDelete(scope.$index, scope.row)"
-          >删除
-          </el-button>
-          <el-button
-              size="small"
-              type="danger"
-              icon="el-icon-delete"
-              @click="handleDelete(scope.$index, scope.row)"
-          >标记
-          </el-button>
-        </template>
+    <div v-show="scene == 0">
+      <ImageTable/>
+    </div>
 
-      </el-table-column>
-    </el-table>
-
+    <div v-show="scene == 1">
+      <ImageDetail/>
+    </div>
   </div>
 </template>
 
 <script>
-import {getImages} from '@/api/docker'
-import moment from "moment";
+import ImageTable from './ImageTable'
+import ImageDetail from './ImageDetail'
 
 export default {
   name: 'Image',
-
-  data() {
-    return {
-      tableData: [],
+  components: {
+    ImageTable,
+    ImageDetail
+  },
+  data(){
+    return{
+      scene:1
     }
-  },
-  created() {
-    this.getTableData()
-  },
-
-  methods: {
-    handleEdit(){
-
-    },
-    handleDelete(){
-
-    },
-    async getTableData() {
-      const res = await  getImages()
-      const images = res.data.images
-      console.warn(images)
-      for(let image of images){
-        for(let tag of image.RepoTags){
-          let tagArray = tag.split(':');
-          let idArray = image.Id.split(':');
-          let daytime = moment(parseInt(image.Created)*1000).format('YYYY-MM-DD HH:mm:ss')
-
-          this.tableData.push({
-            Id: idArray[1].slice(0, 12),
-            Repository: tagArray[0],
-            Tag: tagArray[1],
-            Created: daytime,
-            Size: `${(image.Size/1000/1000).toFixed(0)} MB`,
-            Labels:  "",
-          })
-
-        }
-      }
-
-
-
-    },
   }
 }
 </script>
