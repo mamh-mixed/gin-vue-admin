a7d7da760568daa887aa882b5d3ec2e37b693e0c
bright.ma <bright.ma@magesfc.com>
2023-08-05 22:06:17 +0800

    web: dockerTools: update BaseInfo table
    Change-Id: I6dad16b781c52e25c87e7f248fa2450aae09bfe2

--- web/src/view/dockerTools/image/ImageDetail/BaseInfo/index.vue
+++ web/src/view/dockerTools/image/ImageDetail/BaseInfo/index.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    基础信息 BaseInfo
+    <el-card>
+      <template #header>
+        <span>基础信息</span>
+      </template>
+
+      <el-table :data="tableData" border :show-header="false">
+        <el-table-column width="200" prop="infoKey"/>
+        <el-table-column width="width" prop="infoValue"/>
+      </el-table>
+
+    </el-card>
+
   </div>
 </template>
 
@@ -11,11 +22,23 @@ export default {
 
   data() {
     return {
+      tableData: [
+        {infoKey: "NAME", infoValue: "nginx:1.25.1"},
+        {infoKey: "ID", infoValue: "sha256:021283c8eb95be02b23db0de7f609d603553c6714785e7a673c6594a624ffbda"},
+        {infoKey: "RepoTags", infoValue: "nginx:1.25.1,harbor.magesfc.com/library/nginx:1.25.1"},
+        {infoKey: "RepoDigests", infoValue: "nginx@sha256:08bc36ad52474e528cc1ea3426b5e3f4bad8a130318e3140d6cfe29c8892c7ef,harbor.magesfc.com/library/nginx@sha256:1bb5c4b86cb7c1e9f0209611dc2135d8a2c1c3a6436163970c99193787d067ea"},
+        {infoKey: "Parent", infoValue: ""},
+        {infoKey: "Comment", infoValue: ""},
+        {infoKey: "CreatAt", infoValue: "2023-07-05 01:24:26"},
+        {infoKey: "DockerVersion", infoValue: "20.10.23"},
+        {infoKey: "Author", infoValue: ""},
+        {infoKey: "Os", infoValue: "linux amd64"},
+        {infoKey: "Size", infoValue: "178.20MB"},
+        {infoKey: "LastTagTime", infoValue: "2023-07-26 16:46:09"},
+      ]
     }
   },
-  methods: {
-
-  }
+  methods: {}
 }
 </script>
 
